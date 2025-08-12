1b489e2789e04dd0e95db19743d7c329c9a3f248
Franz Ruggiero <franz.ruggie@gmail.com>
2025-08-12 09:29:38 -0700

    TI-57,TI-58

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.kotlinSerialization)
+
+    id("app.cash.sqldelight") version "2.0.1"
+    id("io.mockative") version "3.0.1"
 }
 
 kotlin {
@@ -28,9 +31,29 @@ kotlin {
         commonMain.dependencies {
             implementation(libs.kotlinx.coroutines.core)
             implementation(libs.kotlinx.serialization)
+
+            implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.kotlinx.serialization)
+            implementation("io.ktor:ktor-client-core:2.3.4")
+            implementation("io.ktor:ktor-client-cio:2.3.4") // Or android engine
+            implementation("io.ktor:ktor-client-logging:2.3.4")
+            implementation("io.ktor:ktor-client-content-negotiation:2.3.4")
+            implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.4")
+            implementation("app.cash.sqldelight:runtime:2.0.1")
+            implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.5.0")
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
+        }
+
+
+        androidMain.dependencies {
+            implementation("app.cash.sqldelight:android-driver:2.0.1")
+            implementation("androidx.datastore:datastore-preferences:1.0.0")
+        }
+
+        jvmTest.dependencies {
+            implementation("io.mockative:mockative:3.0.1")
         }
     }
 }
--- shared/src/commonMain/kotlin/network/OpenLibraryApi.kt
+++ shared/src/commonMain/kotlin/network/OpenLibraryApi.kt
@@ -1,4 +1,43 @@
 package network
 
+import dev.drivemode.techtest.BookModel
+import dev.drivemode.techtest.Works
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class SubjectResponse(
+    val name: String,
+    @SerialName("work_count") val workCount: Int,
+    val works: List<Worka>
+)
+
+@Serializable
+data class Work(
+    val title: String,
+    @SerialName("cover_id") val coverId: Int? = null,
+    val authors: List<Author>
+)
+
+@Serializable
+data class Author(
+    val name: String
+)
+
 class OpenLibraryApi {
+    private val client = HttpClient {
+        install(ContentNegotiation) {
+            json()
+        }
+    }
+
+    suspend fun getBooksBySubject(subject: String): SubjectResponse {
+        val url = "https://openlibrary.org/subjects/${subject}.json"
+        return client.get(url).body()
+    }
 }
